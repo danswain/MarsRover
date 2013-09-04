f18f2552c0ba0c31117f56433c97388db396067f
danswain <dan.swain@gmail.com>
2013-09-04 20:04:36 +0100

    initial commit

---
+++ test/MarsRover.AcceptanceTests/Features/MarsRoverSquad.feature
@@ -0,0 +1,13 @@
+﻿Feature: MarsRoverSquad
+	In order to control a rover, 
+	As NASA 
+	I want to sends a simple string of letters to the RoverSquadControl
+
+@acceptance
+Scenario: Control 1 Rover in a 5x5 Plateau
+	Given I have a RoverRoverSquadControl
+	And I enter the command 5 5
+	And I enter the command 1 1 N
+	And I enter the command M
+	When I enter the GO command
+	Then the output should be 1 2 N
---
+++ test/MarsRover.AcceptanceTests/Steps/MarsRoverSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MarsRover.AcceptanceTests.Steps
+{
+	[Binding]
+	public class MarsRoverSteps
+	{
+		[Given(@"I have a RoverRoverSquadControl")]
+		public void GivenIHaveARoverRoverSquadControl()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I enter the (.*) command")]
+		[Given(@"I enter the command (.*)")]
+		public void GivenIEnterTheCommand(string command)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"the output should be (.*)")]
+		public void ThenTheOutputShouldBe(string output)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+	}
+}
