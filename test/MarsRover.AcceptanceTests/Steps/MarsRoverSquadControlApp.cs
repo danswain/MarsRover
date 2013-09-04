89e51894be4f10fabe271d5d60d908390fd878a1
danswain <dan.swain@gmail.com>
2013-09-04 20:35:45 +0100

    extracted out marsroversquadcontrol app wrapper

---
+++ test/MarsRover.AcceptanceTests/Steps/MarsRoverSquadControlApp.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace MarsRover.AcceptanceTests.Steps
+{
+	public class MarsRoverSquadControlApp
+	{
+		private Process _marsRoverSquadControlApp;
+
+
+		public MarsRoverSquadControlApp()
+		{
+			_marsRoverSquadControlApp = new Process();
+			_marsRoverSquadControlApp.StartInfo.UseShellExecute = false;
+			_marsRoverSquadControlApp.StartInfo.RedirectStandardOutput = true;
+			_marsRoverSquadControlApp.StartInfo.RedirectStandardInput = true;
+			_marsRoverSquadControlApp.StartInfo.FileName = "MarsRover.SquadControlApp.exe";
+			_marsRoverSquadControlApp.Start();
+			_marsRoverSquadControlApp.WaitForExit();
+		}
+
+		public void InputCommand(string command)
+		{
+			_marsRoverSquadControlApp.StandardInput.Write(command);
+		}
+
+		public string GetOutput()
+		{
+			return _marsRoverSquadControlApp.StandardOutput.ReadToEnd();
+		}
+	}
+}
--- test/MarsRover.AcceptanceTests/Steps/MarsRoverSteps.cs
+++ test/MarsRover.AcceptanceTests/Steps/MarsRoverSteps.cs
@@ -7,46 +7,26 @@ namespace MarsRover.AcceptanceTests.Steps
 	[Binding]
 	public class MarsRoverSteps
 	{
-		private Process _marsRoverSquadControlApp;
+		private MarsRoverSquadControlApp _marsRoverSquadControlApp;
+
 
 		[Given(@"I have a RoverRoverSquadControl")]
 		public void GivenIHaveARoverRoverSquadControl()
 		{
-			MarsRoverSquadControl();
+			_marsRoverSquadControlApp = new MarsRoverSquadControlApp();
 		}
 
 		[When(@"I enter the (.*) command")]
 		[Given(@"I enter the command (.*)")]
 		public void GivenIEnterTheCommand(string command)
 		{
-			InputCommand(command);
+			_marsRoverSquadControlApp.InputCommand(command);
 		}
 
 		[Then(@"the output should be (.*)")]
 		public void ThenTheOutputShouldBe(string output)
 		{
-			Assert.That(GetOutput(), Is.EqualTo(output));
-		}
-
-		private void InputCommand(string command)
-		{
-			_marsRoverSquadControlApp.StandardInput.Write(command);
-		}
-
-		private void MarsRoverSquadControl()
-		{
-			_marsRoverSquadControlApp = new Process();
-			_marsRoverSquadControlApp.StartInfo.UseShellExecute = false;
-			_marsRoverSquadControlApp.StartInfo.RedirectStandardOutput = true;
-			_marsRoverSquadControlApp.StartInfo.RedirectStandardInput = true;
-			_marsRoverSquadControlApp.StartInfo.FileName = "MarsRover.SquadControlApp.exe";
-			_marsRoverSquadControlApp.Start();
-			_marsRoverSquadControlApp.WaitForExit();
-		}
-
-		private string GetOutput()
-		{
-			return _marsRoverSquadControlApp.StandardOutput.ReadToEnd();
+			Assert.That(_marsRoverSquadControlApp.GetOutput(), Is.EqualTo(output));
 		}
 	}
 }
